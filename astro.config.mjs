f489baecfa813c5cf57f6ba894d2e037577bffca
James Wilson <j.w@jameswilson.io>
2025-02-12 12:09:25 +1100

    Removed unused mdx plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [],
 
   vite: {
     plugins: [tailwindcss()],
